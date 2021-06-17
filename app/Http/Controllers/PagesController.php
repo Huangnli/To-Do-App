0f41a512a589844c4a6c714b08004b6c70350d22
Ian Haranaka <ian.hrnk@gmail.com>
2021-06-17 16:42:25 -0400

    atualizando controllers

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Lista;
 
 class PagesController extends Controller
 {
@@ -15,6 +16,7 @@ class PagesController extends Controller
     }
 
     public function index() {
-        return view('pages.index');
+        $listas = Lista::all();
+        return view('pages.index')->with('listas', $listas);
     }
 }
