725e9ab740da758db8295166d3a20c8edb0c5c80
Huangnli <50835276+Huangnli@users.noreply.github.com>
2021-06-24 12:21:45 -0400

    iniciando a tela de login

--- TelaCadastro/myfirstreact/src/paginas/Login.js
+++ TelaCadastro/myfirstreact/src/paginas/Login.js
@@ -1,11 +1,26 @@
-import React, {Component} from 'react'
-import '../index.css'
+import React, { Component } from 'react'
+import '../index.css';
+import '../css/login.css';
+import logo from '../imagem/logo.svg';
+import arte1 from '../imagem/arte1.svg';
 
 class Login extends Component {
     render() {
         return (
-            <div className='App'>
-                <p>Hello World!!</p>
+            <div>
+                <div className='Informacao'>
+                    <div>
+                        <div className='logo'>
+                            <img src={logo} alt="Logo" />
+                        </div>
+                        <div className='Descricao'>
+                            <a>Crie listas e organize suas tarefas dinamicamente.</a>
+                        </div>
+                    </div>
+                    <div className='arte1'>
+                        <img src={arte1} alt="Arte 1" />
+                    </div>
+                </div>
             </div>
         );
     }
