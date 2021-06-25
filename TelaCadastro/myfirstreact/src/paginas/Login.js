2a57aec17276fc51b54f80c1f9a82c8bf18860a1
Huangnli <50835276+Huangnli@users.noreply.github.com>
2021-06-24 21:56:36 -0400

    Ajustes no login

--- TelaCadastro/myfirstreact/src/paginas/Login.js
+++ TelaCadastro/myfirstreact/src/paginas/Login.js
@@ -3,11 +3,12 @@ import '../index.css';
 import '../css/login.css';
 import logo from '../imagem/logo.svg';
 import arte1 from '../imagem/arte1.svg';
+import { Link } from 'react-router-dom'
 
 class Login extends Component {
     render() {
         return (
-            <div>
+            <div className='TelaDeLogin'>
                 <div className='Informacao'>
                     <div>
                         <div className='logo'>
@@ -19,6 +20,44 @@ class Login extends Component {
                     </div>
                     <div className='arte1'>
                         <img src={arte1} alt="Arte 1" />
+                    </div>
+                </div>
+
+                <div className='login'>
+                    <div className="login-group">
+                    <div className='login-titulo'>
+                        <h2>Login</h2>
+                    </div>
+                        <div class="input-group">
+                            <label htmlFor="email">Email:</label>
+                            <input
+                                type="email"
+                                name="email"
+                                id="email"
+                            // value={details.email}
+                            // onChange={e => setDetails({ ...details, email: e.target.value })}
+                            />
+                        </div>
+
+                        <div class="input-group">
+                            <label htmlFor="password">Senha:</label>
+                            <input
+                                type="password"
+                                name="password"
+                                id="password"
+                            // value={details.password}
+                            // onChange={e => setDetails({ ...details, password: e.target.value })}
+                            />
+                        </div>
+
+                        <button>Entrar</button>
+                    </div>
+
+                    <div className="registrar">
+                        <a>Não possui uma conta? </a>
+                        <Link to='/'>
+                            <a>Registre-se</a>
+                        </Link>
                     </div>
                 </div>
             </div>
